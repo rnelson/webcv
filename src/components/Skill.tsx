6342de56d71d7a2db6fec87cc267013fd3185018
Ross Nelson <ross.nelson@gmail.com>
2025-04-22 10:10:49 -0500

    Shrink the skills text on mobile

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -16,10 +16,10 @@ function Skills(skills: SkillList) {
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-6">
                     {
                         skills.skills.map(skill =>
-                            <article key={skill.name} className="skill bg-zinc-300 text-zinc-800 dark:bg-zinc-200 dark:text-zinc-800 rounded-3xl p-4 text-xl lg:text-2xl justify-center">
+                            <article key={skill.name} className="skill bg-zinc-300 text-zinc-800 dark:bg-zinc-200 dark:text-zinc-800 rounded-3xl p-4 text-md lg:text-2xl justify-center">
                                     <div className="skill-header">
                                         {skill.fakey?.length && (
-                                            <i className={`text-sky-400 dark:text-sky-500 text-2xl lg:text-3xl pe-2 fa-thin ${skill.fakey}`}></i>
+                                            <i className={`text-sky-400 dark:text-sky-500 text-md lg:text-3xl pe-2 fa-thin ${skill.fakey}`}></i>
                                         )}
                                         <span className="skill-name font-medium">{skill.name}:</span>
                                     </div>
