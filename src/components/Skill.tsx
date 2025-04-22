e5621c5acaae4401996869a204bfb65cbf898c8d
Ross Nelson <ross.nelson@gmail.com>
2025-04-22 10:28:37 -0500

    More small phone tweaks (and shrinking the text on lg:)

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -12,25 +12,27 @@ export interface SkillList {
 function Skills(skills: SkillList) {
     return (
         <section id="skills">
-            <div className="container grid p-2">
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-6">
-                    {
-                        skills.skills.map(skill =>
-                            <article key={skill.name} className="skill bg-zinc-300 text-zinc-800 dark:bg-zinc-200 dark:text-zinc-800 rounded-3xl p-4 text-md lg:text-2xl justify-center">
-                                    <div className="skill-header">
-                                        {skill.fakey?.length && (
-                                            <i className={`text-sky-400 dark:text-sky-500 text-md lg:text-3xl pe-2 fa-thin ${skill.fakey}`}></i>
-                                        )}
-                                        <span className="skill-name font-medium">{skill.name}:</span>
-                                    </div>
-                                    <div className="skill-body">
-                                        <div className="skill-items font-light">
-                                            {skill.list ? skill.items.join(", ") : skill.items}
+            <div className="flex justify-center">
+                <div className="container place-items-center p-2">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-6">
+                        {
+                            skills.skills.map(skill =>
+                                <article key={skill.name} className="skill bg-zinc-300 text-zinc-800 dark:bg-zinc-200 dark:text-zinc-800 rounded-3xl p-4 text-md lg:text-xl">
+                                        <div className="skill-header">
+                                            {skill.fakey?.length && (
+                                                <i className={`text-sky-400 dark:text-sky-500 pe-2 fa-thin ${skill.fakey}`}></i>
+                                            )}
+                                            <span className="skill-name font-medium">{skill.name}:</span>
                                         </div>
-                                    </div>
-                            </article>
-                        )
-                    }
+                                        <div className="skill-body">
+                                            <div className="skill-items font-light">
+                                                {skill.list ? skill.items.join(", ") : skill.items}
+                                            </div>
+                                        </div>
+                                </article>
+                            )
+                        }
+                    </div>
                 </div>
             </div>
         </section>
