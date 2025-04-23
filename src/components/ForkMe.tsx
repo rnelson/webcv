e7f31d7ed1d89a54a95b8c70de60dd9f31895475
Ross Nelson <ross.nelson@gmail.com>
2025-04-22 21:34:19 -0500

    Let's only show the fork banner on lg and larger I don't want it obscuring actual text

--- src/components/ForkMe.tsx
+++ src/components/ForkMe.tsx
@@ -4,8 +4,7 @@
             href="https://github.com/rnelson/webcv/fork"
             target="_blank"
             rel="noopener noreferrer"
-            className="fixed top-0 left-0 z-[100] cursor-pointer hidden md:inline"
-        >
+            className="fixed top-0 left-0 z-[100] cursor-pointer hidden lg:inline">
             <img alt="Fork this project!" src={"/forkme.svg"} width={150} height={150} />
         </a>
     );
