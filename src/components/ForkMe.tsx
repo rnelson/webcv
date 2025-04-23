f8819032ed48f17c7298174781bf8ae18dfc4d61
Ross Nelson <ross.nelson@gmail.com>
2025-04-22 21:57:13 -0500

    Make the fork banner look better in light mode

--- src/components/ForkMe.tsx
+++ src/components/ForkMe.tsx
@@ -4,7 +4,7 @@
             href="https://github.com/rnelson/webcv/fork"
             target="_blank"
             rel="noopener noreferrer"
-            className="fixed top-0 left-0 z-[100] cursor-pointer hidden lg:inline">
+            className="fixed top-0 left-0 z-[100] cursor-pointer hidden lg:inline brightness-150 dark:brightness-100">
             <img alt="Fork this project!" src={"/forkme.svg"} width={150} height={150} />
         </a>
     );
